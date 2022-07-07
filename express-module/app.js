63c7c2b32bce7cbbd9380492fd63724594b38afa
tahuh <tahuh1124@gmail.com>
2022-07-07 21:37:30 +0900

    use of session

--- express-module/app.js
+++ express-module/app.js
@@ -4,39 +4,22 @@ const express = require('express');
 const fs = require("fs");
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 // create server application
 const app = express();
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:false}));
-
-app.get('/', (req, res) =>{
-    if(req.cookies.auth) {
-        res.send("<h1>Login!</h1>");
-    } else {
-        res.redirect("/login");
-    }
-});
-
-app.get('/login', (req, res) =>{
-    fs.readFile("login.html", (err, data) => {
-        if(err){console.log(err)}
-        res.send(data.toString());
+app.use(
+    session({
+        secret :'secret key',
+        resave: false,
+        saveUninitialized: true
     })
-});
+);
 
-app.post('/login', (req, res) =>{
-    const username = req.body.username;
-    const password = req.body.password;
-
-    if(username == "huh" && password == "1234") {
-        res.cookie('auth' , true);
-        res.redirect('/');
-    } else{
-        res.redirect('/login');
-    }
-});
+app.use((req, res) => {})
 
 app.listen(3000, () =>{
     console.log('express server is running at port 3000')
