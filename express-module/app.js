dc2661f436e0b7b915d5fe427cc082828efe464d
tahuh <tahuh1124@gmail.com>
2022-07-07 21:27:27 +0900

    nodejs

--- express-module/app.js
+++ express-module/app.js
@@ -1,21 +1,41 @@
 // The express is similar to that of the 
 // http server but has many advantages and utilities
 const express = require('express');
+const fs = require("fs");
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 // create server application
 const app = express();
 
-// set-up the event listener
-// or 'routing'
-app.use((req, res) => {
-    const outputArray = [];
-    for(var i = 0; i < 10; i++) {
-        outputArray.push({
-            count:i,
-            name:'name - ' + i
-        })
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/', (req, res) =>{
+    if(req.cookies.auth) {
+        res.send("<h1>Login!</h1>");
+    } else {
+        res.redirect("/login");
     }
-    res.send(outputArray);
+});
+
+app.get('/login', (req, res) =>{
+    fs.readFile("login.html", (err, data) => {
+        if(err){console.log(err)}
+        res.send(data.toString());
+    })
+});
+
+app.post('/login', (req, res) =>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if(username == "huh" && password == "1234") {
+        res.cookie('auth' , true);
+        res.redirect('/');
+    } else{
+        res.redirect('/login');
+    }
 });
 
 app.listen(3000, () =>{
